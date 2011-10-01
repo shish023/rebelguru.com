a9fff6da1780d6db924ebc38fe0f73bfa00be85a
Mohit Thatte <mohit.thatte@gmail.com>
2011-10-01 15:23:34 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RebelguruCom::Application.config.secret_token = '6512384114438ef998e2d937846ae901b0ac081b3832a55584544947d16810f44eca84d2700078eda36f0334de19f183af7e569669443e90dadfc5db73cd8eca'
